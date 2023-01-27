a8039dac2461582a6cc73d31aa2e62531cb0bd6d
limaizabela <contatoizabela@outlook.com>
2023-01-26 23:00:09 -0300

    Desafios intermediários

---
+++ PontosNaCarteira/src/PontosNaCarteira.java
@@ -0,0 +1,31 @@
+import java.util.Scanner; 
+    
+public class PontosNaCarteira {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe a velocidade do veículo: ");
+        int velocidade = leitor.nextInt();
+        System.out.println("Informe a quantidade de multas: ");
+        int multas = leitor.nextInt();
+
+        int contador = 1 + multas;
+
+        if (velocidade > 80 && contador >= 3){
+            System.out.println(contador +" multas. Levou pontos na carteira");
+
+        }
+        if (velocidade > 80 && contador == 2){
+            System.out.println(contador +" multas. Não levou pontos na carteira");
+
+        }
+        if (velocidade == 80 && contador >= 1){
+            System.out.println(multas +" multa. Não levou pontos na carteira");
+
+        }
+
+    }
+    
+}
+
+
+
